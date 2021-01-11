5cbb78ddc37f5dc9a026f4097098aa657affafb0
ShiroKatake <tgkhang262@gmail.com>
2021-01-11 16:16:29 +1100

    ADD: Basic player movement

---
+++ Assets/Scripts/Controls/InputManager.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class InputManager : MonoBehaviour
+{
+	[SerializeField] private float movementSpeed = 10f;
+
+	private GameControls controls;
+	private Vector2 move;
+
+	// Start is called before the first frame update
+	private void Awake()
+    {
+		controls = new GameControls();
+
+		controls.Gameplay.Shoot.performed += context => OnShoot(context);
+
+		controls.Gameplay.Move.performed += context => move = context.ReadValue<Vector2>();
+		controls.Gameplay.Move.canceled += context => move = Vector2.zero;
+    }
+
+	public void OnShoot(InputAction.CallbackContext context)
+	{
+		Debug.Log(context.phase.ToString());
+		//shoot
+	}
+
+	private void Update()
+	{
+		Vector2 m = new Vector2(move.x, move.y) * movementSpeed * Time.deltaTime;
+		transform.Translate(m, Space.World);
+	}
+
+	private void OnEnable()
+	{
+		controls.Gameplay.Enable();
+	}
+}
