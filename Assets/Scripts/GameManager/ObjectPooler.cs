a532d367c6b64ec405c18532e3166037bd56ee84
ShiroKatake <tgkhang262@gmail.com>
2021-01-12 13:05:59 +1100

    ADD: Basic pool for object pool

---
+++ Assets/Scripts/GameManager/ObjectPooler.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjectPooler : MonoBehaviour
+{
+	[System.Serializable]
+	public class Pool
+	{
+		public string tag;
+		public GameObject prefab;
+		public int size;
+	}
+
+	[SerializeField] private List<Pool> pools;
+	public Dictionary<string, Queue<GameObject>> poolDictionary;
+
+	private void Start()
+	{
+		poolDictionary = new Dictionary<string, Queue<GameObject>>();
+	}
+}
