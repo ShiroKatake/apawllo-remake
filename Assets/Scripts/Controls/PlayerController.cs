2cf388b0975e0f1f16865ba58219698d2815cf94
ShiroKatake <tgkhang262@gmail.com>
2021-01-12 14:58:11 +1100

    UPDATE: Commenting

--- Assets/Scripts/Controls/PlayerController.cs
+++ Assets/Scripts/Controls/PlayerController.cs
@@ -1,38 +1,54 @@
 ﻿using UnityEngine;
-using UnityEngine.InputSystem;
 
+/// <summary>
+/// Responses to player inputs.
+/// </summary>
 public class PlayerController : MonoBehaviour
 {
+	#region Serialized Fields
 	[SerializeField] private float movementSpeed = 10f;
+	#endregion
 
+	#region Private Fields
 	private GameControls controls;
 	private Vector2 move;
+	#endregion
 
-	// Start is called before the first frame update
+	/// <summary>
+	/// Subscribe to input events.
+	/// </summary>	
 	private void Awake()
     {
 		controls = new GameControls();
 
-		controls.Gameplay.Shoot.performed += context => OnShoot(context);
+		controls.Gameplay.Shoot.performed += context => OnShoot();
 
 		controls.Gameplay.Move.performed += context => move = context.ReadValue<Vector2>();
 		controls.Gameplay.Move.canceled += context => move = Vector2.zero;
     }
 
-	public void OnShoot(InputAction.CallbackContext context)
+	/// <summary>
+	/// Enables the controls when the controller script is enabled.
+	/// </summary>	
+	private void OnEnable()
 	{
-		Debug.Log(context.phase.ToString());
+		controls.Gameplay.Enable();
+	}
+
+	/// <summary>
+	/// Spawns a bullet when the Shoot event is triggered.
+	/// </summary>
+	public void OnShoot()
+	{
 		ObjectPooler.Instance.SpawnFromPool(Pools.ApawlloBullet, transform.position, transform.rotation);
 	}
 
+	/// <summary>
+	/// Moves the player according to the vector context from the Move event.
+	/// </summary>
 	private void Update()
 	{
 		Vector2 m = new Vector2(move.x, move.y) * movementSpeed * Time.deltaTime;
 		transform.Translate(m, Space.World);
-	}
-
-	private void OnEnable()
-	{
-		controls.Gameplay.Enable();
 	}
 }
