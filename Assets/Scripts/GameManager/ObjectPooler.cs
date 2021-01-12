2359b8df916a8d4ff026cee053acb62aee7baa86
ShiroKatake <tgkhang262@gmail.com>
2021-01-12 14:49:31 +1100

    UPDATE: Commenting

--- Assets/Scripts/GameManager/ObjectPooler.cs
+++ Assets/Scripts/GameManager/ObjectPooler.cs
@@ -1,12 +1,19 @@
-﻿using System.Collections;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using UnityEngine;
+
+/// <summary>
+/// Tags for pools.
+/// </summary>
 
 public enum Pools
 {
 	ApawlloBullet,
 	SquireBullet
 }
+
+/// <summary>
+/// A generic object pooler.
+/// </summary>
 
 public class ObjectPooler : MonoBehaviour
 {
@@ -18,15 +25,31 @@ public class ObjectPooler : MonoBehaviour
 		public int amountToPool;
 	}
 
-	[SerializeField] private List<Pool> pools;
+	#region Private Fields
+	private Dictionary<Pools, Queue<GameObject>> poolDictionary;
+	#endregion
 
-	public Dictionary<Pools, Queue<GameObject>> poolDictionary;
+	#region Serialized Fields
+	[SerializeField] private List<Pool> pools;
+	#endregion
+
+	#region Public Properties
 	public static ObjectPooler Instance { get; private set; }
+	#endregion
+
+	/// <summary>
+	/// Pseudo singleton.
+	/// </summary>
 
 	private void Awake()
 	{
 		Instance = this;
 	}
+
+	/// <summary>
+	/// Initialize pools by instantiating their assigned
+	/// objects and adding them to the dictionary.
+	/// </summary>
 
 	private void Start()
 	{
@@ -46,6 +69,14 @@ public class ObjectPooler : MonoBehaviour
 			poolDictionary.Add(pool.tag, objectPool);
 		}
 	}
+
+	/// <summary>
+	/// Spawns a game object from a specified pool.
+	/// </summary>
+	/// <param name="tag">Which pool to spawn from</param>
+	/// <param name="position">Position to spawn at</param>
+	/// <param name="rotation">Rotation to spawn at</param>
+	/// <returns></returns>
 
 	public GameObject SpawnFromPool (Pools tag, Vector3 position, Quaternion rotation)
 	{
@@ -71,6 +102,12 @@ public class ObjectPooler : MonoBehaviour
 		return objectToSpawn;
 	}
 
+	/// <summary>
+	/// Return an object to a specified pool.
+	/// </summary>
+	/// <param name="tag">Which pool to return to</param>
+	/// <param name="objectToReturn">which object to return</param>
+	/// 
 	public void ReturnToPool(Pools tag, GameObject objectToReturn)
 	{
 		poolDictionary[tag].Enqueue(objectToReturn);
