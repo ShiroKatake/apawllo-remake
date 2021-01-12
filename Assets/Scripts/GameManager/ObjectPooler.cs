0315638d8f2ea4284c3650ee8c34625ebd36cfb7
ShiroKatake <tgkhang262@gmail.com>
2021-01-12 13:20:50 +1100

    UPDATE: Added object spawn function

--- Assets/Scripts/GameManager/ObjectPooler.cs
+++ Assets/Scripts/GameManager/ObjectPooler.cs
@@ -13,10 +13,44 @@ public class ObjectPooler : MonoBehaviour
 	}
 
 	[SerializeField] private List<Pool> pools;
+
 	public Dictionary<string, Queue<GameObject>> poolDictionary;
 
 	private void Start()
 	{
 		poolDictionary = new Dictionary<string, Queue<GameObject>>();
+
+		foreach (Pool pool in pools)
+		{
+			Queue<GameObject> objectPool = new Queue<GameObject>();
+
+			for (int i = 0; i < pool.size; i++)
+			{
+				GameObject obj = Instantiate(pool.prefab);
+				obj.SetActive(false);
+				objectPool.Enqueue(obj);
+			}
+
+			poolDictionary.Add(pool.tag, objectPool);
+		}
+	}
+
+	public GameObject SpawnFromPool (string tag, Vector3 position, Quaternion rotation)
+	{
+		if (!poolDictionary.ContainsKey(tag))
+		{
+			Debug.LogWarning("Pool with tag" + tag + "doesn't exist.");
+			return null;
+		}
+
+		GameObject objectToSpawn = poolDictionary[tag].Dequeue();
+
+		objectToSpawn.SetActive(true);
+		objectToSpawn.transform.position = position;
+		objectToSpawn.transform.rotation = rotation;
+
+		poolDictionary[tag].Enqueue(objectToSpawn);
+
+		return objectToSpawn;
 	}
 }
