077353ac79e0f1457b27977e901374e92ebbd874
ShiroKatake <tgkhang262@gmail.com>
2021-05-12 23:35:50 +1000

    UPDATE: Renamed file from "Ammo" to "UI_Ammo"

---
+++ Assets/Scripts/UI/UI_Ammo.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UI_Ammo : MonoBehaviour
+{
+    [SerializeField] private int bulletCount;
+    [SerializeField] private Image ammoX;
+    [SerializeField] private Image fill;
+
+    const int x = 19, y = 20;
+
+    private int BulletCount { 
+        get => bulletCount;
+        set
+        {
+            bulletCount = value;
+            SetAmmoSize();
+        }
+    }
+
+	// Start is called before the first frame update
+	void Start()
+    {
+        Vector2 size = new Vector2(bulletCount * x, y);
+        ammoX.rectTransform.sizeDelta = size;
+        fill.rectTransform.sizeDelta = size;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void SetAmmoSize()
+	{
+        Vector2 size = new Vector2(bulletCount * x, y);
+        ammoX.rectTransform.sizeDelta = size;
+        fill.rectTransform.sizeDelta = size;
+    }
+}
