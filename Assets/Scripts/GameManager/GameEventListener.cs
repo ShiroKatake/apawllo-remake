8ba9289a9bd028eec0cc968ecc508a7707a238c9
ShiroKatake <tgkhang262@gmail.com>
2021-05-13 14:49:03 +1000

    ADD: Game events for separating visual components from mechanic components

---
+++ Assets/Scripts/GameManager/GameEventListener.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class GameEventListener : MonoBehaviour
+{
+    [SerializeField] private GameEvent gameEvent;
+    [SerializeField] private UnityEvent unityEvent;
+
+    private void Awake() => gameEvent.Register(gameEventListener: this);
+    private void OnDestroy() => gameEvent.Deregister(gameEventListener: this);
+    // Update is called once per frame
+    public void RaiseEvent() => unityEvent.Invoke();
+}
